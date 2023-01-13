45df52c7819e9a9dd105c866f92cebea5c0f96a1
7a6179657473 <72371013+7a6179657473@users.noreply.github.com>
2023-01-12 22:41:40 -0800

    edits to readme, main.py

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Webscrape
+
+# this webscraper will be built by me via several differnt resources in addition im going to try to get chatgpt to help
--- webscrape.py
+++ webscrape.py
@@ -1,1 +1,9 @@
 ## created by zayets @ https://github.com/7a6179657473
+# written in python
+
+import requests
+url='https://oxylabs.io/blog'
+response = requests.get(url)
+from bs4 import BeautifulSoup
+soup = BeautifulSoup(response.text, 'html.parser')
+print(soup.title)
